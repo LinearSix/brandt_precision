f70e6c5f2f307a7b3517d9950af82d21d35d307a
linearsix <linearsix@gmail.com>
2019-02-06 15:10:32 -0600

    additions to several pages content - captcha and file uplaod integration

--- documents/hours_log.md
+++ documents/hours_log.md
@@ -51,7 +51,58 @@ fix footer
 * 12-12:30pm added escape feature to portfolio enlargements
 * 8-9:30pm added form submission success and failure messages
 
+## 2019_01_09
+* 11am-2pm Contact form attachment
+
+## 2019_01_11
+* 11am-2pm Contact form attachment
+
+## 2019_01_18
+* 2:30-5:30pm Work on reCaptcha
+
 ## 2019_01_30
-* Format new images for portfolio page
+* 2-6pm Format new images for portfolio page
+
+## 2019_02_02
+* 3-6pm get captcha working with form action
+
+## 2019_02_05
+* 8:30am-2pm migrate changes to live site
+
+## 
+1. You want to add an entry to the portfolio page, the Celazole PBI Fangs (I’m trying to remember what these are)
+Do I already have photos of this or are they forthcoming?
+
+2. You want to add an entry to the portfolio page, a stainless burner tip.
+Again, are there photos of this already?
+
+3. You want to add an entry to the portfolio page, the MMarks 810 Bronze bread grinder center and main part.
+It sounds like we still need a photo of this.
+
+4. You want to add an entry to the portfolio page, a gold plated Berylium Copper thumb nut.
+It sounds like we still need a photo of this.
+
+5. You want the Berylium Copper part description to lead into some text about additional services such as paint, powder coating, 
+electro-polish or any plating or secondary process called for, including having things cleaned and packaged for clean room use.
+(Here, an idea might be to have some of the text in the Berylium Copper description link to a section in the “services” page that outlines these services.)
+
+6. Add these materials to the exotic materials list: Boron Nitride, Tungsten, Ceramic, Be Cu, Nickel 200, Glass-filled Teflon, PEEK, and Glass filled PEEK, 
+Monel, Bronzes, Heavy Metal (what is that?), and Tungsten impregnated graphite.
+
+7. I’m not sure what that spot is on the graphite photo. Jeff was probably eating chips next to it.
+I can tinker with it a bit, but I don’t have Photoshop or any advanced photo tools. I’ll see what I can pull off. 
+
+8. You want to add some text to the Clients section of the “About” page describing reliable on time job completion being a reason clients choose Brandt. 
+
+9. You want to add to the list of industries on the “About” page: Firearm prototyping and custom work, Pharmaceutical,  Defense, oil field, oil exploration, 
+Agriculture, Music and recording industry, movie industry, Automotive, and racing.
+
+You mention BAE (I had to look that up). Do you want to mention that company specifically next to Defense?
+
+10. Possibly mention high-speed machining in the production header of the “Services” section
+
+11. I will investigate what option we have with the map. It is essentially being imported from google, which is how it has actual direction functionality. 
+I don’t actually construct it, I just display it. They do have some options, but I think they’re fairly simple things like the size of the map. 
+Again. I’ll see what options they offer, but it may be limited.
 
 
--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,9 @@
 
 const express = require('express');
 const router = express.Router();
+const request = require('request');
 const nodemailer = require('nodemailer');
+const Recaptcha = require('express-recaptcha').Recaptcha;
 
 // render contact page
 router.get('/contact', (req, res, next) => {
@@ -12,59 +14,90 @@ router.get('/contact', (req, res, next) => {
 
 });
 
-// POST route from contact form
-router.post('/contact_submit', function (req, res, next) {
-  // console.log(`body.file type: ${typeof(req.body.file)}`);
-  // console.log(`body.file: ${req.body.file}`);
-  // console.log(`req.files.attachment type: ${typeof(req.files.attachment)}`);
-  // console.log(`file.attachment.name: ${req.files.attachment.name}`);
-  console.log(`Number of Files ${Object.keys(req.files).length}`)
-  // let fileElement = document.getElementById("file");
-  // inputElement.addEventListener("change", handleFiles, false);
-  // function handleFiles() {
-  //     let fileList = this.files; /* now you can work with the file list */
-  // }
-  let mailOpts, smtpTrans;
-  smtpTrans = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: 'davie@brandtprecision.com', // get this into an environment variable!
-      pass: 'JunkAccount001' // get this into an environment variable!
-    }
-  });
-  mailOpts = {
-    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
-    to: 'linearsix@gmail.com',
-    subject: 'New message from contact form at brandtprecision.com',
-    text: `Name: ${req.body.name}\r\nCompany: ${req.body.company}\r\nPhone: ${req.body.phone}\r\nEmail: ${req.body.email}\r\nSpecs: ${req.body.comment}\r\n`, 
-    attachments: [
-      {
-        filename: req.files.attachment1.name,
-        content: req.files.attachment1.data
-      },
-      {
-        filename: req.files.attachment2.name,
-        content: req.files.attachment2.data
-      },
-      {
-        filename: req.files.attachment3.name,
-        content: req.files.attachment3.data
-      }
-    ]
-  };
-  smtpTrans.sendMail(mailOpts, function (error, response) {
-    if (error) {
-      let selected_link = 'CONTACT_FAILURE';
-      res.render('contact', { selected_link });
-    }
-    else {
+router.post('/contact_submit', (req, res) => {
+  console.log(`g-recaptcha-response: ${req.body["g-recaptcha-response"]}`);
+  let captcha = req.body["g-recaptcha-response"];
+  if(
+    captcha === undefined ||
+    captcha === '' ||
+    captcha === null
+  ){
+    let selected_link = 'CONTACT_FAILURE'
+    res.render('contact', { selected_link })
+  }
+
+  // Secret Key and Verify URL
+  const secretKey = '6Lc48YkUAAAAAHSWF_kgVAaOspjeFKmGFtE9NGgD';
+  const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}&remoteip=${req.connection.remoteAddress}`;
+  console.log(`verifyUrl: ${verifyUrl}`);
+  
+  // Make Request To VerifyURL
+  request(verifyUrl, (err, response, verifyBody) => {
+    verifyBody = JSON.parse(verifyBody);
+    console.log(`recaptchaBody: ${verifyBody}`);
+
+    // If Not Successful
+    if(verifyBody.success !== undefined && !verifyBody.success){
+      let selected_link = 'CONTACT_FAILURE'
+    res.render('contact', { selected_link })
+    } else {
+      // If successful, send the mail
+
+      let attachmentsArray = [];
+      if (req.files.attachment1) {
+        let attachment1 = {
+          filename: req.files.attachment1.name, 
+          content: req.files.attachment1.data
+        }
+        attachmentsArray.push(attachment1);
+      };
+      if (req.files.attachment2) {
+        let attachment2 = {
+          filename: req.files.attachment2.name, 
+          content: req.files.attachment2.data
+        }
+        attachmentsArray.push(attachment2);
+      };
+      if (req.files.attachment3) {
+        let attachment3 = {
+          filename: req.files.attachment3.name, 
+          content: req.files.attachment3.data
+        }
+        attachmentsArray.push(attachment3);
+      };
+
+      let mailOpts, smtpTrans;
+      smtpTrans = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: 'davie@brandtprecision.com', // get this into an environment variable!
+          pass: 'JunkAccount001' // get this into an environment variable!
+        }
+      });
+      console.log(`attachmentsArray: ${attachmentsArray}`)
+      mailOpts = {
+        from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+        to: 'linearsix@gmail.com',
+        subject: 'New message from contact form at brandtprecision.com',
+        text: `Name: ${req.body.name}\r\nCompany: ${req.body.company}\r\nPhone: ${req.body.phone}\r\nEmail: ${req.body.email}\r\nSpecs: ${req.body.comment}\r\n`, 
+        attachments: attachmentsArray
+      };
+      smtpTrans.sendMail(mailOpts, function (error, response) {
+        if (error) {
+          let selected_link = 'CONTACT_FAILURE';
+          res.render('contact', { selected_link });
+        }
+        else {
+          let selected_link = 'CONTACT_SUCCESS';
+          res.render('contact', { selected_link });
+        }
+      });
       let selected_link = 'CONTACT_SUCCESS';
       res.render('contact', { selected_link });
-    }
+    };
   });
 });
-
 
 module.exports = router;
